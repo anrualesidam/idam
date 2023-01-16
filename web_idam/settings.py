f686c6d624c48dcdab55f5640c39f75e59dadcc8
anrualesidam <anruales.idam@gmail.com>
2023-01-16 13:45:10 -0500

    feat(guia_usuario) hosting idam

--- web_idam/settings.py
+++ web_idam/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qr8&8i@(z2a5y1uc@)^-7xr0nav%**g1mnv8&j8!f3nqe%w@en
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['idam-production.up.railway.app']
+ALLOWED_HOSTS = ['idam-production.up.railway.app','idamm.com.co']
 
 
 # Application definition
@@ -130,6 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
