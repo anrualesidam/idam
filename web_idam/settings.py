31e6ef6861c534970e53baba328a507fc9cea6cf
SJuanOG <sjuanortizg@gmail.com>
2023-03-16 19:23:14 -0400

    deploy testing

--- web_idam/settings.py
+++ web_idam/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'idam/static')]
 
 #STATICFILES_DIRS = (
 #    os.path.join(BASE_DIR, 'static'),
